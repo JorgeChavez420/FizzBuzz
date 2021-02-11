7776587cf1618d33113d5882281daef53dfbad30
Jorge Angel Chavez Guzman <kokito83012@hotmail.com>
2021-02-10 20:07:37 -0600

    FizzBuzz Terminado

--- Program.cs
+++ Program.cs
@@ -6,7 +6,139 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           int[] numeros = new int[101];
+
+            
+
+            for (int i = 0; i < numeros.Length; i++)
+            {            
+                if (numeros[i] == 101)
+                {
+                    
+                }
+                else if (numeros[i]%3 == 0 && numeros[i]%5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");  
+                }
+                else if (numeros[i]%3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (numeros[i]%5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                Console.WriteLine(numeros[i]); 
+
+                numeros[0] = 1;
+                numeros[1] = 2;
+                numeros[2] = 3;
+                numeros[3] = 4;
+                numeros[4] = 5;
+                numeros[5] = 6;
+                numeros[6] = 7;
+                numeros[7] = 8;
+                numeros[8] = 9;
+                numeros[9] = 10;
+                numeros[10] = 11;
+                numeros[11] = 12;
+                numeros[12] = 13;
+                numeros[13] = 14;
+                numeros[14] = 15;
+                numeros[15] = 16;
+                numeros[16] = 17;
+                numeros[17] = 18;
+                numeros[18] = 19;
+                numeros[19] = 20;
+                numeros[20] = 21;
+                numeros[21] = 22;
+                numeros[22] = 23;
+                numeros[23] = 24;
+                numeros[24] = 25;
+                numeros[25] = 26;
+                numeros[26] = 27;
+                numeros[27] = 28;
+                numeros[28] = 29;
+                numeros[29] = 30;
+                numeros[30] = 31;
+                numeros[31] = 32;
+                numeros[32] = 33;
+                numeros[33] = 34;
+                numeros[34] = 35;
+                numeros[35] = 36;
+                numeros[36] = 37;
+                numeros[37] = 38;
+                numeros[38] = 39;
+                numeros[39] = 40;
+                numeros[40] = 41;
+                numeros[41] = 42;
+                numeros[42] = 43;
+                numeros[43] = 44;
+                numeros[44] = 45;
+                numeros[45] = 46;
+                numeros[46] = 47;
+                numeros[47] = 48;
+                numeros[48] = 49;
+                numeros[49] = 50;
+                numeros[50] = 51;
+                numeros[51] = 52;
+                numeros[52] = 53;
+                numeros[53] = 54;
+                numeros[54] = 55;
+                numeros[55] = 56;
+                numeros[56] = 57;
+                numeros[57] = 58;
+                numeros[58] = 59;
+                numeros[59] = 60;
+                numeros[60] = 61;
+                numeros[61] = 62;
+                numeros[62] = 63;
+                numeros[63] = 64;
+                numeros[64] = 65;
+                numeros[65] = 66;
+                numeros[66] = 67;
+                numeros[67] = 68;
+                numeros[68] = 69;
+                numeros[69] = 70;
+                numeros[70] = 71;
+                numeros[71] = 72;
+                numeros[72] = 73;
+                numeros[73] = 74;
+                numeros[74] = 75;
+                numeros[75] = 76;
+                numeros[76] = 77;
+                numeros[77] = 78;
+                numeros[78] = 79;
+                numeros[79] = 80;
+                numeros[80] = 81;
+                numeros[81] = 82;
+                numeros[82] = 83;
+                numeros[83] = 84;
+                numeros[84] = 85;
+                numeros[85] = 86;
+                numeros[86] = 87;
+                numeros[87] = 88;
+                numeros[88] = 89;
+                numeros[89] = 90;
+                numeros[90] = 91;
+                numeros[91] = 92;
+                numeros[92] = 93;
+                numeros[93] = 94;
+                numeros[94] = 95;
+                numeros[95] = 96;
+                numeros[96] = 97;
+                numeros[97] = 98;
+                numeros[98] = 99;
+                numeros[99] = 100;
+                numeros[100] = 101;
+                
+                
+                
+
+                
+                         
+            }
         }
     }
 }
